9a1762241ec3cc06eacd5e7c9b49becbe838e3dc
mieel <46019913+mieel@users.noreply.github.com>
2020-12-04 16:34:44 +0100

    Update Get-AppConfigurationKeyValue.ps1

--- powershell/Get-AppConfigurationKeyValue.ps1
+++ powershell/Get-AppConfigurationKeyValue.ps1
@@ -1,56 +1,74 @@
-function Get-AppConfigurationKeyValue {
+$clientId = 'client/app id'
+$tenantId = 'tenant id'
+$secret = 'client secret'
+
+az login --service-principal --username $clientId  --password $secret --tenant $tenantId
+
+function Get-KeyReference {
     param(
-        [string] $Key
+        $string = 'Server=$(Lookup.Database.Server);Database=$(Lookup.Database.Name);Trusted_Connection=Yes'
+        ,
+        $regex = '\$\([\w.]*\)'
+    )
+    $matches = ($string | select-string -pattern $regex -AllMatches).Matches.Value
+    if ($matches) { 
+        $matches.replace('$(','').replace(')','')     
+    }
+}
+
+function Convert-KeyReference {
+    [cmdletbinding()]
+    param(
+        $String
+        ,
+        $Dictionary
+        ,
+        $Prefix = '\$\('
+        ,
+        $Suffix = '\)'
+    )        
+    $stringOutput = $string
+    $references = Get-KeyReference -String $string -Regex "$Prefix[\w.]*$Suffix"
+    if (-not $references) {
+        Return $stringOutput
+    }
+    $references | Foreach-Object {
+        $refKey = $_
+        $refValue = ($Dictionary | Where-Object {$_.key -eq $refkey}).Value
+        Write-Verbose "$refkey resolved to $refValue"
+        $stringOutput = $stringOutput -replace "$Prefix$refKey$Suffix",$refValue            
+    }
+    Convert-KeyReference -String $stringOutput -Dictionary $Dictionary           
+}
+
+function Get-AppConfigurationKeyValue {
+    [cmdletbinding()]
+    param(
+        [string] $Key = '*'
         ,
         [string] $Store
         ,
         [string] $Label = '*'
         ,
-        [switch] $ResolveSecret
+        [switch] $NoResolveSecret
+        ,
+        [switch] $ExcludeNoLabel
     )   
     # az appconfig kv list -h
-    If ($ResolveSecret) {
-        $kv = '--resolve-keyvault'
+    If (-not $NoResolveSecret) {
+        $resolveKv = '--resolve-keyvault'
     } 
-    $json = az appconfig kv list --name $Store --label $Label --key $Key $kv
+    $Output = az appconfig kv list --name $Store --label \0 --key $Key $resolveKv | ConvertFrom-Json
+    $Output += az appconfig kv list --name $Store --label $Label --key $Key $resolveKv | ConvertFrom-Json
 
-    $kvlist = $json | ConvertFrom-Json 
-
-    ForEach($kv in $kvlist) {
-             
-        @{ }
+    ForEach ($kv in $Output) {
+        $value = $kv.value       
+        $value = Convert-KeyReference -String $value -Dictionary $Output
+        Write-Output @{ $kv.key = $value}
     }
 }
 
-
-$store = 'mystore'
-$Key = 'mykey'
-(Get-AppConfigurationKeyValue  -Store $store -Key $Key -Label Acceptance).Value
-
-(Get-AppConfigurationKeyValue  -Store $store -Key $Key -Label Acceptance -ResolveSecret).Value
-
-Measure-Command {
-    Write-Host 'all keys'
-    Get-AppConfigurationKeyValue  -Store $store -Key * -Label Acceptance
-}
-
-Measure-Command {
-    Write-Host 'all keys'
-    Get-AppConfigurationKeyValue  -Store $store -Key Common* -Label Acceptance
-}
-
-Measure-Command {
-    Write-Host 'single key'
-    Get-AppConfigurationKeyValue  -Store $store -Key $Key -Label Acceptance
-}
-
-
-Measure-Command {
-    Write-Host 'single resolved secret'
-    Get-AppConfigurationKeyValue  -Store $store -Key $Key -Label Acceptance -ResolveSecret
-}
-
-Measure-Command {
-    Write-Host 'multi resolved secret'
-    Get-AppConfigurationKeyValue  -Store $store -Key MyProduct* -Label Acceptance -ResolveSecret
-}
+$store = 'MyAppConfig'
+$Key = '*'
+$label = 'Production'
+Get-AppConfigurationKeyValue  -Store $store -Key $Key -Label $label
