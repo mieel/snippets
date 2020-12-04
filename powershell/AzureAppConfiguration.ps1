f648c8b17106c39122c7261ba15b12b45da8088d
mieel <46019913+mieel@users.noreply.github.com>
2020-12-04 10:49:51 +0100

    Create AzureAppConfiguration.ps1

---
+++ powershell/AzureAppConfiguration.ps1
@@ -0,0 +1,57 @@
+
+function Get-AppConfigurationKeyValue {
+    param(
+        [string] $Key
+        ,
+        [string] $Store
+        ,
+        [string] $Label = '*'
+        ,
+        [switch] $ResolveSecret
+    )   
+    # az appconfig kv list -h
+    If ($ResolveSecret) {
+        $kv = '--resolve-keyvault'
+    } 
+    $json = az appconfig kv list --name $Store --label $Label --key $Key $kv
+
+    $kvlist = $json | ConvertFrom-Json 
+
+    ForEach($kv in $kvlist) {
+             
+        @{ }
+    }
+}
+
+
+$store = 'imb-autoconnect-config'
+$Key = 'Carwizard.Rest.Api.IIS.UserPassword'
+(Get-AppConfigurationKeyValue  -Store $store -Key $Key -Label Acceptance).Value
+
+(Get-AppConfigurationKeyValue  -Store $store -Key $Key -Label Acceptance -ResolveSecret).Value
+
+Measure-Command {
+    Write-Host 'all keys'
+    Get-AppConfigurationKeyValue  -Store $store -Key * -Label Acceptance
+}
+
+Measure-Command {
+    Write-Host 'all keys'
+    Get-AppConfigurationKeyValue  -Store $store -Key Common* -Label Acceptance
+}
+
+Measure-Command {
+    Write-Host 'single key'
+    Get-AppConfigurationKeyValue  -Store $store -Key $Key -Label Acceptance
+}
+
+
+Measure-Command {
+    Write-Host 'single resolved secret'
+    Get-AppConfigurationKeyValue  -Store $store -Key $Key -Label Acceptance -ResolveSecret
+}
+
+Measure-Command {
+    Write-Host 'multi resolved secret'
+    Get-AppConfigurationKeyValue  -Store $store -Key Webdiv* -Label Acceptance -ResolveSecret
+}
